217c2f6a4c2bf27fb97807d462fa15e6ad1f9013
umubyeyi dinah <dinahu2020@gmail.com>
2018-12-14 12:09:38 +0200

    I have change in javasript

---
+++ file.js
@@ -0,0 +1,26 @@
+
+
+sideA = parseInt(document.Triangle_Inputs.sideA.value);
+sideB = parseInt(document.Triangle_Inputs.sideB.value);
+sideC = parseInt(document.Triangle_Inputs.sideC.value);
+    
+var sideA ;
+var sideB ;
+var sideC;
+Triangle_Inputs = FormName
+   if (sideA + sideB > sideC && sideA + sideC > sideB && sideB+sideC> sideA){
+   }
+   alert("Is a Triangle")
+   
+    if (sideA == sideB && sideB == sideC) {
+    }
+     alert("Equilateral");
+    if (sideA != sideB == sideC) {
+    }
+    alert("Isosceles");
+    if (sideA != sideB != sideC != sideA) {
+    alert("Scalene!");    
+
+    }
+
+    
