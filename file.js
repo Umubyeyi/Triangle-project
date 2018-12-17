59842ead9fefba9fc4b6877f92f0725b67135d03
umubyeyi dinah <dinahu2020@gmail.com>
2018-12-17 08:53:06 +0200

    I add some change in javascript

--- file.js
+++ file.js
@@ -6,20 +6,21 @@ function herTriangle() {
         
    
     
-      
+        if((side1+side2)>side3 && (side1+side3)>side2 && (side2+side3)>side1){
         if (side1 ===side2 && side2 === side3 && side1 ===side3) {
            console.log(alert("This triangle is Equilateral"));
         }
            
        else if(side1 === side2 && side2 !== side3 && side1 !== side3) {
          alert("this triangle is Isosceles");
+         window.location.href="eqltl.html"
         }
  
    
       else if (side1 !== side2 && side1 !== side3 && side2!== side3) {
         alert(" This triangle is Scalene!"); 
         }
-
+      }
      else {
 alert("is no triangle")
    }
