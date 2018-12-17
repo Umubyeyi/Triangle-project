c4874dbde43a4269a80a0910d277702bc60a3243
umubyeyi dinah <dinahu2020@gmail.com>
2018-12-17 10:40:24 +0200

    I add some file in of shapes in my project

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 
-function herTriangle() {
+function checkInputs() {
     side1 = parseInt(document.getElementById('side1').value);
     side2 = parseInt(document.getElementById('side2').value);
     side3 = parseInt(document.getElementById('side3').value);
@@ -9,16 +9,20 @@ function herTriangle() {
         if((side1+side2)>side3 && (side1+side3)>side2 && (side2+side3)>side1){
         if (side1 ===side2 && side2 === side3 && side1 ===side3) {
            console.log(alert("This triangle is Equilateral"));
+           window.location.href="eqltl.html"  
         }
            
        else if(side1 === side2 && side2 !== side3 && side1 !== side3) {
          alert("this triangle is Isosceles");
-         window.location.href="eqltl.html"
+         window.location.href="isoscls.html"
+         
+         
         }
  
    
       else if (side1 !== side2 && side1 !== side3 && side2!== side3) {
         alert(" This triangle is Scalene!"); 
+        window.location.href="scln.html"
         }
       }
      else {
